7faa6dbbeaaf28df6b1cff470783656789dc1ba2
Javy011021 <garciaj1246@gmail.com>
2024-02-03 10:38:57 -0500

    feat obj_function + gradient_method

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 import random
+import numpy as np
 
-data = {'dia': 23, '5anteriores': [45,20,38,25,32]}
+data = {'day': 23, '5pastDays': [45,20,38,25,32]}
 
-def alt_function(min: int = 0, max: int = 1, intv: int = 0.2):
-    return format(round(random.uniform(min, max) / intv) * intv, '.2f')
+def alt_function(min: int = 0, max: int = 1, intv: int = 0.2, num_vals: int = 5):
+    return [round(random.uniform(min, max) / intv) * intv for _ in range(num_vals)]
 
 
-def obj_function( dia_actual: int, dias_ant: [], vals: []):
-    print(alt_function())
+def obj_function( currentDay: int, pastDays: [], vals: []):
+    error = 0
+    predict = 0
+    for i in range(len(pastDays)):
+        predict += pastDays[i]*vals[i]
+    error = (currentDay-predict) ** 2 
+    return error
+
+def gradient(currentDay, pastDays, vals):
+    m = len(pastDays)
+    gradient_vals = np.zeros(m)
     
-    # predict = 0
-    # for i in range(len(dias_ant)):
-    #     predict += dia_actual[i]*vals[i]
+    predict = np.dot(pastDays, vals)
+    error = currentDay - predict
+    
+    for i in range(m):
+        gradient_vals[i] = -2 * error * pastDays[i]
+    
+    return gradient_vals
 
-    # return dia_actual - predict
+def gradient_descent(currentDay, pastDays, vals, learning_rate=0.0001, num_iterations=1000):
+    m = len(pastDays)
+    vals = np.array(vals)
+    
+    for _ in range(num_iterations):
+        grad = gradient(currentDay, pastDays, vals)
+        vals = vals - learning_rate * grad
+    
+    return vals.tolist()
 
-for i in range(10):
-    obj_function(1,[],[])
+vals = alt_function()
+print("Valores originales: ", vals)
+
+result = obj_function(23, [45,20,38,25,32], vals)
+
+print("Error con valores sin optimizar: ",result)
+
+optimized_vals = gradient_descent(23,[45,20,38,25,32], vals)
+
+print("Valores optimizados", optimized_vals)
+
+optimzed_result = obj_function(23,[45,20,38,25,32],optimized_vals)
+
+print("Error con valores optimizados: ", optimzed_result)
