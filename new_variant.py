591b5ae5f325978e06012b23db310e0954fd8a99
Laura Gonzalez <laura.gonzalez@avangenio.com>
2024-02-28 16:10:36 -0500

    feat: modify function to obtain heuristic solution

--- new_variant.py
+++ new_variant.py
@@ -1,18 +1,22 @@
 import random
+from decimal import Decimal
 import numpy as np
 
 ITERATIONS = 20
 DAYS = [23, 24, 25]
 PAST_DAYS = [[45,20,38,25,32],[46,21,39,26,33], [47,22,40,27,34]]
+INTERVAL = 0.2
 
 
+def exact_add(*nbs):
+    return float(sum([Decimal(str(nb)) for nb in nbs]))
 
 
-def alt_function(min: float = 0, max: float = 1, intv: int = 0.2, num_vals: int = 5):
+def alt_function(min: float = 0, max: float = 1, intv: int = INTERVAL, num_vals: int = 5):
     return [float(f'{round(random.uniform(min, max) / intv) * intv:.2f}') for _ in range(num_vals)]
 
 
-def obj_function(vals: []):
+def obj_function(vals: list):
     error = 0
     for i in range(len(DAYS)):
         predict = 0
@@ -36,20 +40,18 @@ def best_alt_solution():
         
 
 
-def h_solution(iterations: int = 1000):
+def h_solution(iterations: int = 10000):
     current_solution = alt_function()
-    current_solution = sorted(current_solution)
+    print(current_solution)
     current_error = obj_function(current_solution)
 
     for _ in range(iterations):
         neighbor_solution = current_solution.copy()
         random_index = random.randint(0, len(neighbor_solution) - 1)
-        change = random.uniform(-0.1, 0.1)
+        change = random.choice([INTERVAL*-1,INTERVAL])
 
-        # Asegurar que el cambio no haga que el valor sea negativo
         if neighbor_solution[random_index] + change >= 0:
-            neighbor_solution[random_index] += change
-            neighbor_solution = sorted(neighbor_solution)
+            neighbor_solution[random_index] = exact_add(neighbor_solution[random_index], change)
             neighbor_error = obj_function(neighbor_solution)
 
             if neighbor_error < current_error:
@@ -61,7 +63,7 @@ def h_solution(iterations: int = 1000):
 
 def test ():
     l = alt_function()
-    r = obj_function([0.02, 0.8, 1.5, 0.5, 0.92])
+    r = obj_function(l)
     print(f'valores: {l}')
     print(f'error: {r}')
 
