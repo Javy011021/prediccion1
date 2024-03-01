41d2ed121e432576eed943c38593a511f318f020
Laura Gonzalez <laura.gonzalez@avangenio.com>
2024-02-29 22:09:07 -0500

    feat: testing simple metaheuristics

---
+++ heuristics.py
@@ -0,0 +1,59 @@
+from MHPython.MH240210 import set_problem, \
+                     set_default_parameters, print_parameters, set_parameters, \
+                     print_solution, systematicSearch, \
+                     mh_RandomSearch, mh_RandomWalk, \
+                     mh_HillClimbing, mh_LocalSearch
+                    #  mh_EvolutionStrategy, mh_GeneticAlgorithm, \
+                    #  execute_mh, compare_search_procedures, print_results
+                    
+from rain_problem import present_problem, obj_function, random_solution, heuristic_solution, \
+    random_change, not_random_change, random_combination
+
+present_problem()
+
+set_problem(obj_function,present_problem, random_solution, heuristic_solution, 
+            random_change, not_random_change,random_combination)
+
+
+'''
+# ----------------------------------------------------------------------
+# General configuration of the search procedures: By default
+# ----------------------------------------------------------------------
+OBJECTIVE_MAX   = True       # goal of the optimization, True: maximization, False: minimization
+MAX_TRIALS      = 1000       # maximum number of solutions to be explored by each metaheuristic
+ECHO            = False      # printing some traces of the run
+GENERATION_SIZE =  10        # number of generations, for P-metaheuristics
+BEST_REFERENCES =   4        # number of solutions considered in the construction of the next generation, for P-metaheuristics
+GENERATIONAL    =  False     # type of replacement in P-metaheuristics, True: generational, False: SteadyState
+SYSTEMATIC_S_INI=  True      # Systematic search, True: From an arbitrary initial solution, False: not random solution
+RUNS=1                       # Repetitions of the metaheuristics
+CRITERION = 'TA'             # 'TA': Treshold accepting, 'RRT': Record-to-Record Travel
+TRESHOLD = 1                 # For TA and RRT
+TRIALS_BEFORE_RESTART = 50   # For Local Search, trials before restart the search
+'''
+
+parameters = {'ECHO': True, 'MAX_TRIALS': 10, 'OBJECTIVE_MAX':False}
+set_parameters(parameters)
+
+print('\nExecute Random Search')
+solRS = mh_RandomSearch()
+print('\nSolutions obtained by Random Search')
+print(solRS)
+
+print('\n---------------------------------------------------------------------------------------------\n')
+print('Execute Random Walk')
+solRW = mh_RandomWalk()
+print('\nSolutions obtained by Random Walk')
+print(solRW)
+
+print('\n---------------------------------------------------------------------------------------------\n')
+print('Execute Hill Climbing')
+solHC = mh_HillClimbing()
+print('\nSolutions obtained by Hill Climbing')
+print(solHC)
+
+print('\n\n')
+print('Compare the results of the three search procedures')
+print('RS:',solRS, 'with evalution',obj_function(solRS))
+print('RW:',solRW, 'with evalution',obj_function(solRW))
+print('HC:',solHC, 'with evalution',obj_function(solHC))
---
+++ rain_problem.py
@@ -0,0 +1,97 @@
+import random
+import numpy as np
+from decimal import Decimal
+from MHPython.operators import uniform_crossover
+
+ITERATIONS = 20
+DAYS = [23, 24, 25]
+PAST_DAYS = [[45,20,38,25,32],[46,21,39,26,33], [47,22,40,27,34]]
+INTERVAL = 0.2
+
+Presentation  = 'Rain Problem'
+def present_problem():
+    print('---------------------------------------------------------------------------------------------')
+    print(Presentation)
+    # print(f'LIST_LENGTH = {LIST_LENGTH}, CAPACITY = {CAPACITY}, MAX_VALUE = {MAX_VALUE}, VALUES_RND = {VALUES_RND}')
+    # print(f'Profits and sizes of the items = {prof_size}')
+    print('---------------------------------------------------------------------------------------------')
+    
+
+def exact_add(*nbs):
+    return float(sum([Decimal(str(nb)) for nb in nbs]))
+
+def random_value(min: float = 0, max: float = 1, intv: int = INTERVAL):
+    return float(f'{round(random.uniform(min, max) / intv) * intv:.2f}')
+    
+def obj_function(vals: list):
+    error = 0
+    for i in range(len(DAYS)):
+        predict = 0
+        for j in range(len(PAST_DAYS[i])):
+            predict += PAST_DAYS[i][j]*vals[j]
+        error += abs((DAYS[i]-predict))
+    return error
+
+def random_solution(min: float = 0, max: float = 1, intv: int = INTERVAL, num_vals: int = 5):
+    return [random_value(min=min, max=max, intv=intv) for _ in range(num_vals)]
+
+def heuristic_solution(iterations: int = 10000):
+    current_solution = random_solution()
+    print(current_solution)
+    current_error = obj_function(current_solution)
+
+    for _ in range(iterations):
+        neighbor_solution = current_solution.copy()
+        random_index = random.randint(0, len(neighbor_solution) - 1)
+        change = random.choice([INTERVAL*-1,INTERVAL])
+
+        if neighbor_solution[random_index] + change >= 0:
+            neighbor_solution[random_index] = exact_add(neighbor_solution[random_index], change)
+            neighbor_error = obj_function(neighbor_solution)
+
+            if neighbor_error < current_error:
+                current_solution = neighbor_solution
+                current_error = neighbor_error
+
+    print(f'Mejor solución: {current_solution} \t Error: {current_error}')
+    return current_solution
+
+
+# the value of a random position is randomly changed
+def random_change(solution, MIN_VALUE=0, MAX_VALUE=10,): 
+    random_index = random.randint(0, len(solution) - 1)
+    solution[random_index] = random_value(min=MIN_VALUE, max=MAX_VALUE)
+    return solution
+
+def not_random_change(solution):
+    pass
+
+# each value is chosen randomly from any of the solutions
+def random_combination(solution1, solution2):
+    return uniform_crossover(solution1, solution2)
+
+
+#Test Functions
+def test ():
+    l = random_solution()
+    r = obj_function(l)
+    print(f'valores: {l}')
+    print(f'error: {r}')
+
+def best_alt_solution():
+    solution = random_solution()
+    min_error = obj_function(solution)
+    for i in range(ITERATIONS):
+        actual_sol = random_solution()
+        error = obj_function(actual_sol)
+        if error < min_error:
+            min_error = error
+            solution = actual_sol
+    
+    print(f'solucion: {solution} \t error:{min_error}')
+    return [solution]
+
+# best_alt_solution()
+# test()
+# heuristic_solution()
+
